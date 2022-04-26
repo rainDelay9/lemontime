9b9cd5beea6cafa2a6e95c838e7429dbc21d26f1
matan.orland <orlmatan@amazon.com>
2022-04-26 15:24:13 +0300

    add body validator

--- src/lib/cdk-stack.ts
+++ src/lib/cdk-stack.ts
@@ -56,10 +56,70 @@ export class LemonTimeStack extends Stack {
             },
         });
 
+        /*   const userModel: apigateway.Model = api.addModel('UserModel', {
+         *         schema: {
+         *             type: apigateway.JsonSchemaType.OBJECT,
+         *             properties: {
+         *                 userId: {
+         *                     type: apigateway.JsonSchemaType.STRING
+         *                 },
+         *                 name: {
+         *                     type: apigateway.JsonSchemaType.STRING
+         *                 }
+         *             },
+         *             required: ['userId']
+         *         }
+         *     });
+         *     api.root.addResource('user').addMethod('POST',
+         *         new apigateway.LambdaIntegration(userLambda), {
+         *             requestModels: {
+         *                 'application/json': userModel
+         *             }
+         *         }
+         *     );
+         * */
+
+        const newTimerModel = api.addModel('NewTimerModel', {
+            schema: {
+                type: apigw.JsonSchemaType.OBJECT,
+                properties: {
+                    hours: {
+                        type: apigw.JsonSchemaType.INTEGER,
+                    },
+                    minutes: {
+                        type: apigw.JsonSchemaType.INTEGER,
+                    },
+                    seconds: {
+                        type: apigw.JsonSchemaType.INTEGER,
+                    },
+                    url: {
+                        type: apigw.JsonSchemaType.STRING,
+                    },
+                },
+                required: ['hours', 'minutes', 'seconds', 'url'],
+            },
+        });
+
         const timers = api.root.addResource('timers');
         timers.addMethod(
             'POST',
-            new apigw.LambdaIntegration(postTimersBackend, {})
+            new apigw.LambdaIntegration(postTimersBackend, {
+                passthroughBehavior: apigw.PassthroughBehavior.NEVER,
+            }),
+            {
+                requestModels: {
+                    $default: newTimerModel,
+                },
+                requestValidator: new apigw.RequestValidator(
+                    this,
+                    'post-request-validator',
+                    {
+                        requestValidatorName: newTimerModel.modelId,
+                        restApi: api,
+                        validateRequestBody: true,
+                    }
+                ),
+            }
         );
 
         timers.addMethod(
